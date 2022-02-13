024ec1e3b0b9d2c9a53034a136f252dd4822102e
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-02-13 23:11:10 +0100

    recreated verdaccio-v6-https-portal

--- libs/verdaccio/v6/https-portal/src/lib/verdaccio-v6-https-portal.spec.ts
+++
@@ -1,7 +0,0 @@
-import { verdaccioV6HttpsPortal } from './verdaccio-v6-https-portal';
-
-describe('verdaccioV6HttpsPortal', () => {
-  it('should work', () => {
-    expect(verdaccioV6HttpsPortal()).toEqual('verdaccio-v6-https-portal');
-  });
-});
