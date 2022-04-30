3d5e9193fa8555af2547de644060b1de792f70d2
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-04-30 21:12:12 +0200

    nx migrate --run-migrations

--- libs/verdaccio/v5/reverse-proxy/jest.config.ts
+++ libs/verdaccio/v5/reverse-proxy/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'verdaccio-v5-reverse-proxy',
-  preset: '../../../../jest.preset.js',
+
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -12,4 +12,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/verdaccio/v5/reverse-proxy',
+  preset: '../../../../jest.preset.ts',
 };
